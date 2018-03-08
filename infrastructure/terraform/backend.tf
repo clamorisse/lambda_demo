ad921aaf3d45ba4ab442da42d5339da59972c71b
Berenice Cotero <berenice.cotero@clarivate.com>
2018-03-07 22:59:46 -0500

    sleep after test for demo

--- infrastructure/terraform/backend.tf
+++ infrastructure/terraform/backend.tf
@@ -6,7 +6,7 @@
 terraform {
   backend "s3" {
     bucket  = "tfstate_lambda_clarivate"
-    key     = "lambda_s3_triggered/dev/terraform.tfstate"
+    key     = "lambda_s3_triggered/local/terraform.tfstate"
     region  = "us-east-1"
     profile = "default"
   }
