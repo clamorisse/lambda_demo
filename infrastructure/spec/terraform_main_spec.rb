e0fc5321de56e6b1f24f7814e8d4a35767f919c2
Berenice Cotero <berenice.cotero@clarivate.com>
2018-03-08 05:39:22 -0500

    corrected tests

--- infrastructure/spec/terraform_main_spec.rb
+++ infrastructure/spec/terraform_main_spec.rb
@@ -7,17 +7,17 @@ end
 
 describe lambda('lambda_s3_triggered-function') do
   it { should exist }
-  its (:role) { should eq 'lambda_s3_triggered-lambda-exec_role'}
-end
-
-describe s3_bucket('source_bvc_filesanalyzed') do
-  it { should exist }
-  it { should have_object('data/CA4PExc1500.csv') }
-  it { should have_object('data/CA4PExc1500.txt') }
+  its (:role) { should eq 'arn:aws:iam::815233088423:role/lambda_s3_triggered-lambda-exec_role'}
 end
 
 describe s3_bucket('source_bvc_files') do
   it { should exist }
   it { should have_object('data/CA4PExc1500.csv') }
-  it { should_not have_object('data/CA4PExc1500.csv') }
+  it { should have_object('data/CA4PExc1500.txt') }
+end
+
+describe s3_bucket('source_bvc_filesanalyzed') do
+  it { should exist }
+  it { should have_object('data/CA4PExc1500.csv') }
+  it { should_not have_object('data/CA4PExc1500.txt') }
 end
