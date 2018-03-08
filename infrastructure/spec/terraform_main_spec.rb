9887a0a4a829d134ca0e5f3352e0b8c90fad2d42
Berenice Cotero <berenice.cotero@clarivate.com>
2018-03-07 22:42:12 -0500

    more tests and typo

--- infrastructure/spec/terraform_main_spec.rb
+++ infrastructure/spec/terraform_main_spec.rb
@@ -7,10 +7,15 @@ end
 
 describe s3_bucket('source_bvc_filesanalyzed') do
   it { should exist }
-#  it { should have_object('lambda_s3_triggered/dev/terraform.tfstate') }
+  it { should have_object('data/CA4PExc1500.csv') }
 end
 
 describe s3_bucket('source_bvc_files') do
   it { should exist }
-#  it { should have_object('lambda_s3_triggered/dev/terraform.tfstate') }
+  it { should have_object('data/CA4PExc1500.csv') }
+end
+
+describe lambda('lambda_s3_triggered-function') do
+  it { should exist }
+  its (:role) { is_expected.to eq('lambda_s3_triggered-lambda-exec_role') }
 end
