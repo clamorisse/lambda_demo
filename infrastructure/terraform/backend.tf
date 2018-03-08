4dbb69e9d503b812e21b8de616a6b5e40f1aa8f5
Berenice Cotero <berenice.cotero@clarivate.com>
2018-03-07 19:03:32 -0500

    upgrading functionality of terraform

---
+++ infrastructure/terraform/backend.tf
@@ -0,0 +1,14 @@
+#########################################
+#      Configure the Remote state       #
+#########################################
+
+
+terraform {
+  backend "s3" {
+    bucket  = "tfstate_lambda_clarivate"
+    key     = "lambda_s3_triggered/dev/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "bvc"
+  }
+}
+
